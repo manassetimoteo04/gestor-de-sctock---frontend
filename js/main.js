57cb56bcc9157e41c84060f14144dc71b7e10e5d
manassetimoteo04 <manassetimoteo4@gmail.com>
2024-05-06 11:47:10 +0100

    New javascript features commit

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 // MOSTRAR E GUARDANDO O MENU
+// import { callback } from "chart.js/dist/helpers/helpers.core.js";
 import { productFunction } from "./products.js";
-
 productFunction();
 
+// EXECUTAR A FUNÇÃO DE MOSTAR E FECHAR O MENU
 const toggleMenuBtn = document.querySelector(".toggleMenu__btn");
 const body = document.querySelector("body");
 
@@ -10,10 +11,18 @@ toggleMenuBtn.addEventListener("click", function () {
   body.classList.toggle("menu-hidden");
 });
 
+// MOSTRAR PERFIL QUANDO CLICADO NA FOTO DE PERFIL
+const profile = document.querySelector(".header__profile-box");
+profile.addEventListener("click", function () {
+  document.body.classList.toggle("show");
+});
+
+// FUNÇÃO PARA ACTIVAR O MODO ESCURO
+// const setDark = () => );
 const darkModeFunction = function () {
   const btnToggleDarkMode = document.querySelector(".toggle-dark-mode");
-  const root = document.documentElement;
   btnToggleDarkMode.addEventListener("click", function () {
+    const root = document.documentElement;
     root.classList.toggle("dark-mode");
     document.querySelector(".light-icon").classList.toggle("hidden");
     document.querySelector(".dark-icon").classList.toggle("hidden");
@@ -21,14 +30,75 @@ const darkModeFunction = function () {
     localStorage.setItem("darkMode", isDarkMode);
   });
 };
-darkModeFunction();
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.documentElement;
   const isDarkMode = root.classList.contains("dark-mode");
-  localStorage.setItem("darkMode", isDarkMode);
+
   const DarkMode = localStorage.getItem("darkMode") === "true";
-  if (DarkMode) root.classList.add("dark-mode");
+  if (DarkMode) {
+    root.classList.add("dark-mode");
+  } else {
+    root.classList.remove("dark-mode");
+  }
 });
+darkModeFunction();
+
+// CRIANDO O COUNTER
+const labelPurchase = document.querySelectorAll(".tot-label-num");
+const counter = function (content) {
+  const number = +content.textContent;
+  let starter = 0;
+  let numberCounter;
+  // if (number > 1000000) {
+  //   numberCounter = function () {
+  //     starter += ;
+  //     content.textContent = starter;
+  //   };
+  // }
+  // if (number > 100000) {
+  //   numberCounter = function () {
+  //     starter += 5000;
+  //     content.textContent = starter;
+  //   };
+  // }
+  // if (number > 10000) {
+  //   numberCounter = function () {
+  //     starter += 500;
+  //     content.textContent = starter;
+  //   };
+  // }
+
+  numberCounter = function () {
+    starter += 100;
+    content.textContent = starter;
+  };
+
+  const timer = setInterval(() => {
+    numberCounter();
+    if (starter > number) clearInterval(timer);
+  }, 0);
+};
+
+const callBack = function (entries, obs) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+  body.scroll(function (e) {
+    const target = closest(`.${entry.target.classList[1]}`);
+    if (!target) return;
+    console.log(entry);
+  });
+  // counter(entry.target.outer);
+  // obs.unobserve(entry.target);
+};
+const observer = new IntersectionObserver(callBack, {
+  root: null,
+  threshold: 0,
+});
+labelPurchase?.forEach((element) => {
+  // observer.observe(element);
+  counter(element);
+});
+
 // FUNÇÃO PARA A PÁGINA INICIAL DO DASHBOARD
 const dashBoardFunction = function () {
   const ctx = document.getElementById("myChart");
